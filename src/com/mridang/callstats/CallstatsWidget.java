4d9a4140480fdc9e27414791625cead1ae6e88f4
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-01-06 12:54:59 +0200

    Upgraded libraries. Made the Dashclock data world-readable by adding support for 2.0 API. Some cleanup and tweaks. Reset and externalized API key.

--- src/com/mridang/callstats/CallstatsWidget.java
+++ src/com/mridang/callstats/CallstatsWidget.java
@@ -4,9 +4,9 @@ import java.util.Calendar;
 import java.util.Random;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.preference.PreferenceManager;
@@ -45,7 +45,7 @@ public class CallstatsWidget extends DashClockExtension {
 
 		super.onCreate();
 		Log.d("CallstatsWidget", "Created");
-		BugSenseHandler.initAndStartSession(this, "a83e054b");
+		BugSenseHandler.initAndStartSession(this, getString(R.string.bugsense));
 
 	}
 
@@ -211,10 +211,13 @@ public class CallstatsWidget extends DashClockExtension {
 				} catch (NameNotFoundException e) {
 
 					Integer intExtensions = 0;
+				    Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
+				    String strPackage;
 
-					for (PackageInfo pkgPackage : mgrPackages.getInstalledPackages(0)) {
+				    for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
 
-						intExtensions = intExtensions + (pkgPackage.applicationInfo.packageName.startsWith("com.mridang.") ? 1 : 0); 
+				    	strPackage = info.serviceInfo.applicationInfo.packageName;
+						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0); 
 
 					}
 
